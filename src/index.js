05889cfe3b185f5e0b50d53eb187227a5deabf66
Peter Schussheim <peter@schussheim.com>
2017-10-17 16:32:38 -0400

    feat(releases): update scripts to automate build and publishing to npm

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-// @flow
-
 /**
- * Credit for inspiration and original source: https://codesandbox.io/s/AGRRMl63.
- * 
- * Makes it easier to create a glamorous component which
- * accepts props to enable/disable certain styles.
- *
- * accepts an object where the key is a prop and the value
- * is the styles that should be applied if that prop is
- * passed. Returns a function which you pass to a
- * glamorousComponentFactory.
- *
  * @param {Object} styles The prop to styles object
  * @return {Function} the dynamic styles function
  */
